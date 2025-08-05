7597f5a9e74a1fd477fbf1a5f87ecf69f25fa1af
pedro <stropro31@gmail.com>
2025-08-05 19:55:00 +0200

    add domain api to cors allower

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.use(morgan("dev"))
 app.use(express.json({ limit: '50mb' }))
 
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: ["http://localhost:3000","https://selectividad.mumbx.com"], 
     credentials: true
 }))
 
